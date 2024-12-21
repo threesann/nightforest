08b59cb19fbe1ce5c827a0825865e3c28a48fddb
Rishabh Sandhu <171568557+threesann@users.noreply.github.com>
2024-12-21 21:22:32 +0000

    added back link to hoverpage temp

--- src/pages/hoverpage.js
+++ src/pages/hoverpage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Hoverpage() {
     const name = "david";
@@ -6,6 +7,9 @@ function Hoverpage() {
     return (
         <div className="flex flex-col justify-center items-center w-4/5 h-screen m-auto bg-theme-deskblue">
             <p className="text-4xl">yo</p>
+            <Link to="/">
+                <p className="hover:underline">go back</p>
+            </Link>
         </div>
     );
 }
