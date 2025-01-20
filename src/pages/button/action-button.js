a92fde44d917e27a2c106e0b0770ea1c9cc1cb6f
Rishabh Sandhu <171568557+threesann@users.noreply.github.com>
2025-01-20 14:30:27 +0000

    mobile scaling & textbox rework

---
+++ src/pages/button/action-button.js
@@ -0,0 +1,18 @@
+
+
+function ActionButton({children, label, ...props}) {
+    return (
+        <button
+          className=
+            "group relative aspect-square h-full px-2 flex items-center justify-center bg-theme-deskblue hover:bg-theme-active hover:text-white transition-colors"
+            {...props}
+        >
+        {children}
+          <div className="absolute w-fit -top-8 bg-theme-active px-3 py-1 pointer-events-none opacity-0 left-0 group-hover:opacity-100 transition-opacity z-30 overflow-hidden shadow shadow-black/50">
+            <p className="translate-y-4 group-hover:translate-y-0 transition-all text-nowrap">{label}</p>
+          </div>
+        </button>
+      );
+}
+
+export default ActionButton;
--- src/pages/storefront.js
+++ src/pages/storefront.js
@@ -1,83 +1,119 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import ActionButton from "./button/action-button";
+
 function Storefront() {
   const [discovered, setDiscovered] = useState(false);
   const [money, setMoney] = useState(0);
   const [showTextbox, setShowTextbox] = useState(false);
 
+  const [showBlinker, setShowBlinker] = useState(false);
+  let [textboxContent, setTextboxContent] = useState("")
+
   let name = "David";
 
   function updateMoney(amt) {
-    if (money + amt < 0) // cant afford check
+    if (money + amt < 0) // negative check
       return setMoney(0) 
     
     setMoney(money + amt)
   }
 
+  function textboxClick(element) {
+    return new Promise(resolve => {
+        const handleClick = () => {
+            element.removeEventListener('click', handleClick);
+            resolve();
+        };
+        element.addEventListener('click', handleClick);
+    });
+}
+
+  function textboxClear() {
+    setShowTextbox(false)
+    setShowBlinker(false)
+    setTextboxContent("")
+  }
   
   async function displayText(text, speed) {
-    setShowTextbox(true)
-    await new Promise(r => setTimeout(r, 100)); // small timeout to let program process textbox appearance
-    let textbox = document.getElementById("textbox")
-    textbox.innerHTML = ""
+    setShowTextbox(true);
+
+    await new Promise(r => setTimeout(r, 100)); // small pause for effect
     for (let i=0; i < text.length; i++) {
       let letter = text.charAt(i);
-      textbox.textContent += letter
+      setTextboxContent(prevContent => prevContent + letter)
       await new Promise(r => setTimeout(r, speed));
         }
-    // blinking cursor code here
+
+    setShowBlinker(true)
+    await textboxClick(document.querySelector('.textbox'))
+    textboxClear()
+    
   }
 
+  // find a way to set speed to 0/skip textbox animation on click during for loop
+
+  async function shopIntro() {
+    await displayText("hello there, "+name+".", 50)
+    await displayText("welcome to the store.", 50)
+    await displayText("i am the shopkeeper.", 50)
+    await displayText("why not take a look around? i have many items for sale.", 50)
+  }
 
   return (
     <div className='flex flex-col gap-2 items-center justify-center w-4/5 h-screen m-auto'> {/* central column */}
-      <div className='flex justify-center items-center h-16'>
-        <h1 className='text-theme-deskblue drop-shadow-theme text-7xl'>{discovered ? "storefront" : "??????"}</h1>
+      <div className='flex w-full justify-center h-16'>
+        <h1 className='text-theme-deskblue drop-shadow-theme md:text-7xl text-6xl'>{discovered ? "storefront" : "??????"}</h1>
       </div>
       {/* image zone */}
-      <div className='relative w-[1280px] h-[724px] border-4 border-theme-deskblue drop-shadow-theme'> 
-        <img src="/assets/storefront/desk-larger.png" alt="black" className='absolute w-full h-full'/>
-        <img src="/assets/storefront/bushes-larger.png" alt="bushes" className='absolute w-full h-full' />
-        <img src="/assets/storefront/empty_shelves-larger.png" alt="bushes" className='absolute w-full h-full' />
-        <img src="/assets/storefront/shelf_jar-larger.gif" alt="bushes" className='absolute w-full h-full' />
+      <div className='grid border-4 border-theme-deskblue drop-shadow-theme'> 
+        <img src="/assets/storefront/desk-larger.png" alt="black" className='row-start-1 col-start-1 w-full h-full'/>
+        <img src="/assets/storefront/bushes-larger.png" alt="bushes" className='row-start-1 col-start-1 w-full h-full' />
+        <img src="/assets/storefront/empty_shelves-larger.png" alt="shelves" className='row-start-1 col-start-1 w-full h-full' />
+        <img src="/assets/storefront/shelf_jar-larger.gif" alt="jar" className='row-start-1 col-start-1 w-full h-full' />
         <div className='flex justify-start absolute right-2 top-2 gap-2 px-4 py-2 bg-black border-4 border-theme-deskblue w-fit'> {/* money box */}
-          <img src="/assets/coin_placeholder.png" alt="money" className='size-7'/>
+          <img src="/assets/coin_placeholder.png" alt="money" className='md:size-7 size-6'/>
           <span className='text-white text-xl'>{money}</span>
         </div>
       </div>
 
       {/* button zone */}
-      <div className='flex justify-between w-[1280px]'>
-        <div className='flex grid grid-cols-6 gap-2 w-fit h-full items-center drop-shadow-theme'>
-          <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2 h-full' onClick={() => setDiscovered(true)}>look closer</button>
-          <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2 h-full' onClick={() => setDiscovered(false)}>forget</button>
-          <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2 h-full' onClick={() => updateMoney(50)}>+50</button>
-          <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2 h-full' onClick={() => updateMoney(-50)}>-50</button> 
-          <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2 h-full' onClick={() => displayText("hello there, "+name, 100)}>speak</button>
-          <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2 h-full' onClick={() => setShowTextbox(!showTextbox)}>the box</button>
+      <div className='flex flex-col md:flex-row md:justify-between gap-2 w-full'>
+        <div className='flex grid grid-cols-6 gap-2 w-full md:w-fit h-full items-center drop-shadow-theme'>
+          <ActionButton label="look closer" onClick={() => setDiscovered(true)}>
+            <img src="/assets/storefront/buttons/icon_lookcloser-alt.png" className="size-12 filter hover:invert" />
+          </ActionButton>
+          <ActionButton label="forget" onClick={() => setDiscovered(false)}>
+            <img src="/assets/storefront/buttons/icon_forget.png" className="size-12 filter hover:invert" />
+          </ActionButton>
+          <ActionButton label="+50" onClick={() => updateMoney(50)}>+50</ActionButton>
+          <ActionButton label="-50" onClick={() => updateMoney(-50)}>-50</ActionButton> 
+          <ActionButton label="speak" onClick={() => displayText("hello there, "+name+".", 100)}>speak</ActionButton>
+          <ActionButton label="dialogue" onClick={() => shopIntro()}>dialogue</ActionButton>
         </div>
-        <div className="flex flex-col">
-          <p className="text-theme-deskblue">{name}</p>
-          <p className="text-theme-deskblue">BITCHES: 0</p>
+        <div className="flex flex-row gap-2 items-center">
+          <p className="text-theme-deskblue text-4xl">{name}</p>
+          <div className="h-10 w-0.5 bg-theme-deskblue block" />
+          <div className="flex flex-col max-h-full">
+            <p className="text-theme-deskblue">LEVEL 1</p>
+            <p className="text-theme-deskblue">BITCHES: 0</p>
+          </div>
         </div>
 
-        <Link to="/hoverpage">
+        <Link to="/hoverpage" className="w-fit h-fit">
           <button className='bg-theme-deskblue hover:bg-theme-active text-black px-2'>
             elsewhere
           </button>
         </Link>
       </div>
       {showTextbox && 
-      <div onClick={() => setShowTextbox(false)} className="flex absolute bottom-20 w-3/5 h-1/5 p-2 border-4 border-theme-deskblue bg-black hover:cursor-pointer">
-        <span id="textbox" className='text-white text-2xl'></span>
-      </div>
+        <div className="textbox flex justify-between absolute bottom-20 md:w-3/5 w-4/5 h-1/5 p-2 border-4 border-theme-deskblue bg-black hover:cursor-pointer">
+          <span className='text-white text-2xl'>{textboxContent}</span>
+          {showBlinker && <span className="text-white text-2xl place-self-end">{">"}</span>}
+        </div>
       }
     </div>
-      
-      
-      
-
       
   );
 }
